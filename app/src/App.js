3b88032cde631731bac51fb2c6527eb0254a7b24
PreetS12301~ <preetbhardwaj517@gmail.com>
2022-09-19 14:14:47 +0530

    practise

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Main } from './React-fetch/Main';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
--- app/src/React-fetch/Pages/Css.jsx
+++ app/src/React-fetch/Pages/Css.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export const Css = () => {
+  
   return (
-    <div>Css</div>
+    <div>
+
+    </div>
   )
 }
