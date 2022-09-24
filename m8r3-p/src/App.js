b30fb1e848d024409074bbf66996806aa011fbd4
PreetS12301~ <preetbhardwaj517@gmail.com>
2022-09-24 06:33:43 +0530

    setup-done

---
+++ m8r3-p/src/App.js
@@ -0,0 +1,13 @@
+
+import { Heading } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Setup done</Heading>
+    </div>
+  );
+}
+
+export default App;
