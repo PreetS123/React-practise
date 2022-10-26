c9ac91d012e210e819a2fb9e119488acf5006041
PreetS123 <preetbhardwaj517@gmail.com>
2022-10-26 19:02:02 +0530

    fetching-and-posting-data-with-axios

--- crud_app_axios/src/Todo.jsx
+++ crud_app_axios/src/Todo.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import axios from "axios";
+import { nanoid } from "nanoid";
+import React, { useState, useEffect } from "react";
 
 export const Todo = () => {
+  const [data, setData] = useState([]);
+  const [inpdata, setInpData] = useState("");
 
-    
+  let api = `http://localhost:8080/posts`;
+
+   //////////////// Posting with axios ////////////////////////
+
+  const handleClick = () => {
+    let value = inpdata.trim();
+    addTodo(value);
+  };
+  const addTodo = (value) => {
+    let obj = {
+      title: value,
+      completed: false,
+    };
+    axios
+      .post(api, obj)
+      .then(r=>fetchData(r))
+      .catch((e) => console.log(e));
+  };
+  /////////////////////// Fetching code with axios ////////////////////////
+
+  const fetchData=()=>{
+    axios.get(api)
+    .then(r=>setData(r.data))
+    .catch(e=>console.log(e));
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[data.length])
+//   console.log(data)
+
+
   return (
     <>
-        <div className="addtodo"></div>
+      <div className="addtodo">
+        <input type="text" onChange={(e) => setInpData(e.target.value)} />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <div className="mapdata">
+        {
+            data.length>0 && data?.map(item=>{
+                return (
+                    <div key={nanoid()}>
+                       <h5>{item.id}</h5>
+                       <h4>{item.title}</h4>
+                    </div>
+                )
+            })
+        }
+      </div>
     </>
-  )
-}
+  );
+};
