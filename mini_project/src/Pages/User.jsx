f79b249fce66b36ec2967d76e822d1439770715b
PreetS123 <preetbhardwaj517@gmail.com>
2022-10-27 13:49:27 +0530

    searching-done-with-axios

--- mini_project/src/Pages/User.jsx
+++ mini_project/src/Pages/User.jsx
@@ -7,29 +7,41 @@ import { OneCard } from "../Component/OneCard";
 export const User = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
+  const [text,setText]= useState('');
+  const [querry,setQuerry]= useState('');
 
   let api = "http://localhost:8080/candidates";
 
-  function fetchData(page) {
+  function fetchData(page,querry) {
     axios({
       method:'get',
       url:api,
        params: {
         _limit:5,
-        _page:page
+        _page:page,
+         q:querry
       }})
       .then((r) => setData(r.data))
       .catch((e) => console.log(e));
   }
 
   useEffect(() => {
-    fetchData(page);
-  }, [data.length,page]);
+    fetchData(page,querry);
+  }, [page,querry]);
 
-  console.log(data.length);
+  // console.log(data.length);
+   
+  const handleClick=(querry)=>{
+    setQuerry(querry)
+  }
+
 
   return (
     <>
+    <SearchBarWrapper>
+      <input type="text"  value={text} onChange={(e)=>setText(e.target.value)}  />
+      <button onClick={()=>handleClick(text)}>Search</button>
+    </SearchBarWrapper>
       <UserDivWrapper>
         {data?.map((item) => {
           return <OneCard key={nanoid()} {...item} />;
@@ -53,3 +65,13 @@ const UserDivWrapper = styled.div`
   height: fit-content;
   padding: 2%;
 `;
+
+ const SearchBarWrapper=styled.div`
+ display: flex;
+ justify-content:center;
+ align-items: center;
+ gap: 20px;
+ width:70%;
+ height:40px;
+ padding:2%;
+ `;
