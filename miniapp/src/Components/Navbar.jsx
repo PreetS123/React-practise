853ec658c766d3e90b3fac42ee5c40449698b8a3
PreetS123 <preetbhardwaj517@gmail.com>
2022-10-29 17:17:38 +0530

    navbar-css-completed

--- miniapp/src/Components/Navbar.jsx
+++ miniapp/src/Components/Navbar.jsx
@@ -3,21 +3,41 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
+  const active={
+    fontSize:'30px',
+    textDecoration: 'none',
+    fontWeight:500,
+    color:'#F913B3'
+  }
+  const unactive={
+    fontSize:'20px',
+    textDecoration: 'none',
+    fontWeight:900,
+    
+  }
   return (
     <>
       <div className={styles.navmain}>
         <div>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/"
+           style={({isActive})=>(isActive)?active:unactive}
+          >Home</NavLink>
         </div>
         <div>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/product"
+          style={({isActive})=>(isActive)?active:unactive}
+          >Product</NavLink>
         </div>
         <div className={styles.authdiv}>
           <div >
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/login"
+            style={({isActive})=>isActive?active:unactive}
+            >Login</NavLink>
           </div>
           <div>
-            <NavLink to="/signup">SignUp</NavLink>
+            <NavLink to="/signup"
+            style={({isActive})=>isActive?active:unactive}
+            >SignUp</NavLink>
           </div>
         </div>
       </div>
