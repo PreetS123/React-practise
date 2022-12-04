66a4d9a7af5ce4b6a787af1d7894fedb9342e71b
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-04 10:39:02 +0530

    todo-done

--- todo-react/src/Pages/Todo.jsx
+++ todo-react/src/Pages/Todo.jsx
@@ -2,87 +2,78 @@ import { nanoid } from 'nanoid';
 import React,{useState,useEffect} from 'react'
 
 export const Todo = () => {
-  const [todoData,setTodoData]= useState([]);
-  const [inpVal,setInpVal]=useState({
-     item:"",
-     image:"",
-     completed:false
+  const [todoVal,setTodoVal]= useState([]);
+  const [inpVal,setInpVal]= useState({
+    item:"",
+    image:"",
+    completed:false
   })
+
+   const handleInp=(e)=>{
+    // console.log(e.target.value)
+     setInpVal({...inpVal,[e.target.name]:e.target.value})
+   }
 
 
   ///////////  posting Data //////////////
-  
-  const handleChange=(e)=>{
-    setInpVal({...inpVal,[e.target.name]:e.target.value})
-}
-
-    const handleSubmit=()=>{
-      
-        fetch(`http://localhost:8080/todo`,{
-            method:'POST',
-            body:JSON.stringify(inpVal),
-            headers:{
-                'content-type':'application/json'
-            }
-        })
+    const handleAddTodo=async()=>{
+         await fetch(`http://localhost:8080/todo`,{
+          method:'POST',
+          body:JSON.stringify(inpVal),
+          headers:{
+            'content-type':'application/json'
+          }
+         })
     }
     
    //////// fetching data//////////
-
-    const fetchData=()=>{
-      fetch(`http://localhost:8080/todo`,{
-        method:'get'
-      })
-      .then(r=>r.json())
-      .then(r=>setTodoData(r))
-      .catch(e=>console.log(e))
-    }  
-
-    useEffect(()=>{
-         fetchData();
-    },[todoData.length])
+      const FetchData=async()=>{
+           await fetch(`http://localhost:8080/todo`,{
+            method:'GET'
+           }).then(r=>r.json())
+           .then(r=>{
+            console.log(r)
+            setTodoVal(r)
+           })
+           .catch(er=>{
+            console.log('er in fetching',er)
+           })
+      }
     
-    console.log(todoData);
-
 
     /////// deleting data ///////////////
-    const handleDelete=(id)=>{
-       fetch(`http://localhost:8080/todo/${id}`,{
-        method:'DELETE'
-       }).then(r=>r.json())
-        .then(d=>fetchData(d))
-    }
+    
 
     /////// edit data ///////////////
-     const handleEdit=(id)=>{
-        console.log(id);
-        // fetch(`http://localhost:8080/todo/${id}`,{
-        //   method:'PATCH'
-        // }).then(r=>r.json())
-        // .then(d=>fetchData(d))
-     }
+    
+
+    useEffect(()=>{
+      FetchData();
+    },[])
 
   return (
     <div className='todoMain'>
             {/* add todo section  */}
             <div className='addTodo'>
-                <input type="text" placeholder='add name' name='item'  onChange={handleChange} autoComplete='off'  />
-                <input type="text" placeholder='Add Image' name='image' onChange={handleChange} autoComplete='off' />
-                <button onClick={handleSubmit}>ADD</button>
+                  <input type="text" name='item'  onChange={handleInp} autoComplete='off' />
+                  <input type="url"  name='image' onChange={handleInp} autoComplete='off' />
+                  <button onClick={handleAddTodo} >ADD</button>
             </div>
             
             <div className='todoMapping'>
              <div className='tododatamap'>
-              {
-                todoData?.map(item=>{
-                   return <div key={nanoid()}  className='todoitem'>
-                        <img src={item.image} alt={item.item} />
-                        <p>{item.item}</p>
-                        <button onClick={()=>handleDelete(item.id)}>DELETE</button>
-                        <button onClick={()=>handleEdit(item.id)}>EDIT</button>
-                   </div>
+             {
+                todoVal?.map(item=>{
+                  return(
+                    <div key={nanoid()} className='todoitem'>
+                      <p>{item.item}</p>
+                      <img src={item.image} alt={item.item} />
+                      
+                      <button>DELETE</button>
+                    </div>
+                  )
                 })
-              }
+             }
              </div>
             </div>
     </div>
