2c6384a9aa400e81a37facc28f958c1d567d53d1
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-04 08:12:15 +0530

    timer-practised-again

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,54 +2,53 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  let [stop,setStop]= useState(false);
-  let [hrs,setHrs]= useState(0);
-  let [min,setMin]= useState(0);
-  let [sec,setSec] = useState(0);
-  let [mili,setMili]= useState(0);
+  const [hrs,setHrs]= useState(0);
+  const [min,setMin]= useState(0);
+  const [sec,setSec]= useState(0);
+  const [mili,setMili]= useState(0);
+  const [stop,setStop]= useState(false);
 
-     const onStart=()=>{
-       setStop(true);
-     }
+  const onStop=()=>{
+    setStop(false);
+    setHrs(0);
+    setMin(0);
+    setSec(0);
+    setMili(0);
+  }
+  const onPause=()=>{
+    setStop(false);
+  }
+  const onStart=()=>{
+    setStop(true);
+  }
 
-     const onPause=()=>{
-        setStop(false)
-    }
-    const onStop=()=>{
-       setStop(false);
-       setHrs(0);
-       setMin(0);
-       setSec(0);
-       setMili(0);
-    }
-
-    useEffect(()=>{
-      let interval=null;
+  useEffect(()=>{
+     let interval=null;
+     interval= setInterval(()=>{
       if(stop){
-        interval=setInterval(()=>{
-          if(min>59){
-            setHrs(hrs+1);
-            setMin(0);
-          }
-          if(sec>59){
-            setMin(min+1);
-            setSec(0);
-          }
-          if(mili>99){
-            setSec(sec+1);
-            setMili(0);
-          }
-          if(mili<=99){
-            setMili(mili+1);
-          }else{
-            clearInterval(interval);
-          }
-        },0.001)
-        return ()=>{
-          clearInterval(interval)
+        if(min>59){
+          setHrs(hrs+1);
+          setMin(0)
         }
-      }
-    })
+        if(sec>59){
+          setMin(min+1);
+          setSec(0)
+        }
+        if(mili>99){
+          setSec(sec+1);
+          setMili(0);
+        }
+          if(mili<=99){
+            setMili(mili+1)
+          }else{
+            clearInterval(interval)
+          }
+        }
+     },0.001)
+     return ()=>{
+      clearInterval(interval)
+     }
+  })
 
   return (
     <div className="App">
