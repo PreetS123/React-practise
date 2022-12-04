101dcba548cba89c6cc6e5357fe30bf081360014
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-04 11:59:36 +0530

    timer-done

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,53 +2,54 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [hrs,setHrs]= useState(0);
-  const [min,setMin]= useState(0);
-  const [sec,setSec]= useState(0);
-  const [mili,setMili]= useState(0);
-  const [stop,setStop]= useState(false);
+ const [stop,setStop]= useState(false);
+ const [hrs,setHrs]= useState(0);
+ const [min,setMin]= useState(0);
+ const [sec,setSec]= useState(0);
+ const [mili,setMili]= useState(0);
 
-  const onStop=()=>{
-    setStop(false);
-    setHrs(0);
-    setMin(0);
-    setSec(0);
-    setMili(0);
-  }
-  const onPause=()=>{
-    setStop(false);
-  }
-  const onStart=()=>{
-    setStop(true);
-  }
+ const onStop=()=>{
+  setStop(false);
+  setHrs(0);
+  setMin(0);
+  setSec(0);
+  setMili(0);
+ }
+ const onPause=()=>{
+  setStop(false);
+ }
 
-  useEffect(()=>{
-     let interval=null;
-     interval= setInterval(()=>{
-      if(stop){
-        if(min>59){
-          setHrs(hrs+1);
-          setMin(0)
-        }
-        if(sec>59){
-          setMin(min+1);
-          setSec(0)
-        }
-        if(mili>99){
-          setSec(sec+1);
-          setMili(0);
-        }
-          if(mili<=99){
-            setMili(mili+1)
-          }else{
-            clearInterval(interval)
-          }
-        }
-     },0.001)
-     return ()=>{
+ const onStart=()=>{
+   setStop(true);
+ }
+
+   useEffect(()=>{
+    let interval=null;
+    interval=setInterval(()=>{
+   if(stop){
+    if(min>59){
+      setHrs(hrs+1);
+      setMin(0);
+    }
+    if(sec>59){
+      setMin(min+1);
+      setSec(0);
+    }
+    if(mili>99){
+      setSec(sec+1);
+      setMili(0);
+    }
+    if(mili<=99){
+      setMili(mili+1);
+    }else{
       clearInterval(interval)
-     }
-  })
+    }
+   }
+    },0.001)
+    return ()=>{
+      clearInterval(interval)
+    }
+   })
 
   return (
     <div className="App">
