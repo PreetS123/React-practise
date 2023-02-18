2168cd51a20e783981077987b0e4caa4820a2a10
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-02-18 17:51:01 +0530

    data-fetched-asynchronously

--- reduxtoolkit-basiclearning/src/Components/Home/Home.js
+++ reduxtoolkit-basiclearning/src/Components/Home/Home.js
@@ -1,26 +1,15 @@
 import React,{useEffect} from 'react'
 import MovieListing from '../MovieListing/MovieListing';
-import MovieApi from '../../Common/Api/MovieApi';
-import {APIKey} from '../../Common/Api/MovieApiKey';
 import { useDispatch } from 'react-redux';
-import { addMovies } from '../../Features/movies/movieSlice';
+import { fetchAsyncMovies } from '../../Features/movies/movieSlice';
 
 
 const Home = () => {
     const dispatch= useDispatch();
-    const movieText='Avenger';
 
-    const fetchMovies=async()=>{
-        const response= await MovieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`).catch((err)=>{
-            console.log('Error',err);
-        });
-        dispatch(addMovies(response.data));
-        console.log('The response from API',response);
-     };
-
-    useEffect(()=>{       
-     fetchMovies();
-    },[])
+    useEffect(()=>{     
+        dispatch(fetchAsyncMovies());
+    },[dispatch]);
 
     return (
         <div>
--- reduxtoolkit-basiclearning/src/Features/movies/movieSlice.js
+++ reduxtoolkit-basiclearning/src/Features/movies/movieSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import MovieApi from "../../Common/Api/MovieApi";
+import { APIKey } from "../../Common/Api/MovieApiKey";
+
+let movieText='Harry'
+export const fetchAsyncMovies=createAsyncThunk('movies/fetchAsyncMovies',async()=>{
+    const response= await MovieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+     return response.data
+})
+
 
 const initialState={
     movies:{},
@@ -13,6 +22,18 @@ const movieSlice=createSlice({
             state.movies=payload;
         },
     },
+    extraReducers:{
+        [fetchAsyncMovies.pending]:()=>(
+            console.log('pending')
+        ),
+        [fetchAsyncMovies.fulfilled]:(state,{payload})=>{
+            console.log('Fetched Successfully!')
+            return {...state,movies:payload}
+        },
+        [fetchAsyncMovies.rejected]:()=>{
+            console.log('rejected')
+        },
+    }
 })
 
 export const {addMovies}= movieSlice.actions;
